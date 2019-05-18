0ed9b58dee244c31b5e13182f8b92fe6b7126969
Vercris <alvaro_1399@hotmail.com>
2019-05-18 00:37:45 -0500

    Primera1.0

--- js/vercris.js
+++ js/vercris.js
@@ -1,9 +1,3 @@
-/*Service Worker*/ 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js')
-      .then(reg => console.log('Registro de SW exitoso', reg))
-      .catch(err => console.warn('Error al tratar de registrar el sw', err))
-  };
 /*  ANIMACIONES */
 document.addEventListener('DOMContentLoaded', function() {
                    //  LAS LLAMADAS
