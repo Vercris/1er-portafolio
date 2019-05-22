f652adc12d26321c9fd535ba248da6fff337d8b6
Vercris <alvaro_1399@hotmail.com>
2019-05-22 13:24:38 -0500

    Prueba2

---
+++ js/vercris.js
@@ -0,0 +1,38 @@
+/*  ANIMACIONES */
+document.addEventListener('DOMContentLoaded', function() {
+                   //  LAS LLAMADAS
+    var carousel    = document.querySelectorAll('.carousel');
+    var parallax    = document.querySelectorAll('.parallax');
+    var collapsible = document.querySelectorAll('.collapsible');
+    var boton = document.querySelectorAll('.fixed-action-btn');
+    var slider = document.querySelectorAll('.slider');
+    var sidenav = document.querySelectorAll('.sidenav');
+    
+    //  LAS INSTANCIAS
+    var instances = M.Sidenav.init(sidenav);
+    var  Ins_parallax= M.Parallax.init(parallax);
+    var instances = M.Slider.init(slider,
+        {
+                indicators:false,
+                Duration:5000,
+                interval:5000
+        });
+    var instances = M.FloatingActionButton.init(boton,
+        {
+            hoverEnabled:true,
+        });
+    var  Ins_carousel = M.Carousel.init(carousel,
+        {
+            dist:0,
+                shift:0,
+                indicators:true,
+                noWrap:false,
+        });
+    var Ins_collapsible = M.Collapsible.init(collapsible,
+        {
+            accordion: true,
+            inDuration:250,
+            outDuration:250,
+        }
+        );
+});
